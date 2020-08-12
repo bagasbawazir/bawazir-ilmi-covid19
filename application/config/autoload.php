3f7ba1fb55fab139c900d8c39dad1164b58af5cd
bagasbawazir <bawazirfadhil@gmail.com>
2020-08-12 12:30:53 +0700

    update login

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -60,7 +60,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database','session');
 
 
 /*
@@ -88,7 +88,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('url');
 
 
 /*
@@ -138,3 +138,5 @@ $autoload['language'] = array();
 */
 
 $autoload['model'] = array();
+
+
